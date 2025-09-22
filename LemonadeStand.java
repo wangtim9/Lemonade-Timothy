a433f15e9002761d6b2e8c76f4b932bb3bbc36e8
Wang <wangtim27@7hills.org>
2025-09-22 08:52:46 -0400

    add instance variables

---
+++ LemonadeStand.java
@@ -0,0 +1,26 @@
+public class LemonadeStand{
+	
+	//instance variables
+	int days;
+	double money;
+	int temperature;
+	String weather;
+	int numLemons;
+	int numIceCubes;
+	int numPaperCups
+	int CupsSugar;
+	double priceLemon;
+	double priceIceCubes;
+	double pricePaperCups;
+	double priceCupsSugar;
+	//timothy
+	
+	//constructors
+	public Start()
+		
+	
+	
+	//methods
+	
+	
+}

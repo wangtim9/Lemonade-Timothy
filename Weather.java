2b4544301cca61010ad112e4768896fb6eccbda3
Wang <wangtim27@7hills.org>
2025-09-30 09:24:00 -0400

    weather report

---
+++ Weather.java
@@ -0,0 +1,12 @@
+public static  Weather(){
+		temperature = Math.random()*65 + 25;
+		if temperature < 65{
+			weather = "Cold";
+		}else if 65 < temperature < 85{
+			weather = "Cloudy";
+		}
+		else{
+			weather = "Sunny";
+		}
+		System.out.println("Temp: " + temperature);
+}

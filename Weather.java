4956d9c30015c582d7482d8932dc54f25ffe6e52
wangtim9 <wangtim27@7hills.org>
2025-10-01 20:08:30 -0400

    Update Weather.java

--- Weather.java
+++ Weather.java
@@ -1,12 +1,16 @@
-public static  Weather(){
-		temperature = Math.random()*65 + 25;
+public static Weather(){
+	int temperature;
+	String weather;
+	temperature = Math.random()*65 + 25;
 		if temperature < 65{
 			weather = "Cold";
-		}else if 65 < temperature < 85{
+		}
+		else if 65 <= temperature < 85{
 			weather = "Cloudy";
 		}
-		else{
+		else {
 			weather = "Sunny";
 		}
 		System.out.println("Temp: " + temperature);
+		System.out.println("Weather: " + weather);
 }
